78cc910bb7d705ff7d091369dfca2e796387aa2f
PoornaAshik <ashikedara@gmail.com>
2026-09-15 10:24:08 +0530

    Integrate planner changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['maplibre-gl'],
-  },
   server: {
     port: 3000,
     host: true,
